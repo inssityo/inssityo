6aa312a869123ed0b3e41e98b27fae35c71a3e12
SuviTR <suvi.r>
2021-02-11 17:05:28 +0200

    Icon component with tooltip.

---
+++ src/components/dashboard/bio/Icon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tooltip chat-icon pointer">
+    <i :class="icon"></i>
+    <span class="tooltiptext">{{ tooltipText }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatIcon',
+  props: ['icon', 'tooltipText']
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../../../assets/styles/variables.scss' as v;
+
+.tooltip {
+  //width: 6rem;
+
+  .tooltiptext {
+    opacity: 0.7;
+  }
+}
+.tooltip:not(:last-of-type) {
+  padding-right: 1rem;
+}
+svg {
+  padding: 1rem;
+  background: v.$KAMPurple;
+  border-radius: 50%;
+  width: 1.7rem !important;
+  height: 1.7rem;
+  color: v.$White;
+}
+
+  
+</style>
